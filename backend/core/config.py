f9170550f167652ef4dc255a19f8757dafb09473
Semih Orhan <sorhan3.14@gmail.com>
2023-10-08 18:32:18 +0300

    create db connection

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,12 +1,16 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
 
 
 class Settings:
     PROJECT_NAME: str = "FastBLOG 🔥"
     PROJECT_VERSION: str = "1.0.0"
+
+    DATABASE_URL: str = os.getenv("POSTGRES_URI")
 
 
 settings = Settings()
